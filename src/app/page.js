fe3d85a472149e76a73d8432d12e76c21c63a51d
laiscrz <lais.albino2003@gmail.com>
2023-11-17 09:38:22 -0300

    Add section vantagens

--- src/app/page.js
+++ src/app/page.js
@@ -5,7 +5,8 @@ import './pagehome.scss'
 
 export default function Home() {
   return (
-    <main className='home'>
+    <main>
+      <section  className='home'>
       <div className='home-text'>
         <h4 className='text-h4'> Bem Vindo, Inova - X Care</h4>
         <h1 className='text-h1'>Conheça o LifeCare!</h1>
@@ -15,6 +16,47 @@ export default function Home() {
       <div className='home-img'>
         <img src='/image/lifecare_img.jpg' alt='Relogio LifeCare' />
       </div>
+      </section>
+      <section className='vantagens-lifecare'>
+        <div className='vantagens-title'>
+          <span>Veja</span>
+          <h2>Vantagens do LifeCare</h2>
+          <div className='vantagens-container'>
+            <div className='box'>
+              <div className='box-img'>
+
+                <img src='/image/vantagem_um.jpg'/>
+
+              </div>
+              <h3>Monitoramento Contínuo</h3>
+              <p>A capacidade de monitoramento constante dos sinais vitais, como frequência cardíaca e pressão arterial, contribui para a identificação precoce de problemas de saúde. </p>
+
+            </div>
+          <div className='box'>
+              <div className='box-img'>
+
+                <img src='/image/vantagem_dois.png'/>
+
+              </div>
+              <h3>Geolocalização</h3>
+              <p>A integração da geolocalização ajuda a rastrear a localização do idoso em situações de emergência, melhorando ainda mais a eficácia da resposta.</p>
+
+            </div>
+          <div className='box'>
+              <div className='box-img'>
+
+                <img src='/image/vantagem_tres.jpg'/>
+
+              </div>
+              <h3>Design Acessível e Confortável</h3>
+              <p>Seu design é pensado para atender às preferências estéticas e ergonômicas do público idoso, garantindo que o uso diário seja agradável e sem complicações </p>
+
+            </div>
+          </div>
+
+
+        </div>
+      </section>
     </main>
   )
 }
