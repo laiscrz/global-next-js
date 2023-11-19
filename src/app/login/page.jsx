4a62b5d23528f83d3de9f8df675bfd34ee4b8381
laiscrz <lais.albino2003@gmail.com>
2023-11-18 22:38:45 -0300

    Add page Login

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -1,8 +1,31 @@
+import '/src/app/login/login.scss'
 
-export default function Login(){
-    return(
-        <main>
-            <h2>teste</h2>
+
+
+export default function Login() {
+    return (
+        <main className='main-login'>
+            <div className="login">
+                <div className="login-text">
+                    <h2>Login Rápido para uma <br /> Experiência Personalizada</h2>
+                    <img src="/image/login.png" alt="Foto Login" className='login-img'/>
+                </div>
+                <div className="login-form">
+                    <div className="login-card">
+                        <h2>LOGIN</h2>
+                        <div className="login-input">
+                            <label htmlFor="email">Email</label>
+                            <input type="email" name="email" placeholder="Email" />
+                        </div>
+                        <div className="login-input">
+                            <label htmlFor="senha">Senha</label>
+                            <input type="password" name="senha" placeholder="senha" />
+                        </div>
+                        <button className="btn-login">Entrar</button>
+                    </div>
+                </div>
+
+            </div>
         </main>
     )
 }
