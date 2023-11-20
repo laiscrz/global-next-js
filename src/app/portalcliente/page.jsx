47a31895424c58f96a15c8201b4d0ce91e8145ca
laiscrz <lais.albino2003@gmail.com>
2023-11-20 15:06:40 -0300

    add  encerrando sessao com sessionStorage

--- src/app/portalcliente/page.jsx
+++ src/app/portalcliente/page.jsx
@@ -8,90 +8,116 @@ import { AiFillAlert } from "react-icons/ai";
 import { RiQuestionMark } from "react-icons/ri";
 import { IoWatchOutline } from "react-icons/io5";
 import { RxMagnifyingGlass } from "react-icons/rx";
+import Link from 'next/link';
+import { useState } from 'react';
 
 
 
 export default function PortalCliente() {
-  const login = JSON.parse(sessionStorage.getItem("loginClient")) 
+  const login = JSON.parse(sessionStorage.getItem("loginClient"))
+  const [user, setUser] = useState(login);
 
-    
-  if (!login  || !login.email) {
-    
+  if (!login || !login.email) {
+
     window.location = '/login';
-    return null; 
+    return null;
   }
 
+  const deslogar = () => {
+    try {
+      sessionStorage.removeItem("loginClient");
+      setUser("");
+      alert('Sessão encerrada com sucesso!');
+    } catch (error) {
+      alert('Falha ao encerrar sessão.');
+      console.error('Erro ao encerrar sessão:', error);
+    }
+  };
 
-    const opcoes = [
-        {
-          titulo: 'Cadastrar Dispositivo',
-          descricao: 'Cadastre um novo dispositivo LifeCare.',
-          icone: <IoWatchOutline />,
-        },
-        {
-          titulo: 'Visualizar Dispositivos',
-          descricao: 'Veja todos os dispositivos LifeCare registrados.',
-          icone: <RxMagnifyingGlass />,
-        },
-        {
-          titulo: 'Informações do Cuidador',
-          descricao: 'Visualize informações sobre o seu cuidador.',
-          icone: <RiQuestionMark />,
-        },
-        {
-          titulo: 'Gerenciar Diagnósticos',
-          descricao: 'Visualize e gerencie diagnósticos relacionados ao seu dispositivo.',
-          icone: <FaFileMedicalAlt />,
-        },
-        {
-          titulo: 'Configurações de Conta',
-          descricao: 'Atualize suas informações de conta e preferências.',
-          icone: <FaUserGear />,
-        },
-        {
-          titulo: 'Visualizar Serviços de Manutenção',
-          descricao: 'Visualize serviços de manutenção para o seu dispositivo.',
-          icone: <GrConfigure />,
-        },
-        {
-          titulo: 'Histórico de Serviços de Emergência',
-          descricao: 'Visualize o histórico de serviços de emergência.',
-          icone: <AiFillAlert />,
-        },
-        {
-          titulo: 'Sair',
-          descricao: 'Encerre a sessão no Portal do Cliente.',
-          icone: <ImExit />,
-        },
-      ];
 
-    return (
-        <main>
-            <section className='p-cliente-inicial'>
-                <div className='portal-img'>
-                    <img src="/image/pcliente_inicial.jpg" alt="Cuidador e Cliente com LifeCare" />
-                </div>
-                <div className='portal-text'>
-                    <h2>Seja bem-vindo ao Portal <br /> do Cliente LifeCare !</h2>
-                    <p>Obtenha informações detalhadas sobre o seu dispositivo LifeCare <br /> e gerencie suas opções de maneira simples e intuitiva.</p>
-                </div>
-            </section>
-            <section className='p-cliente-opcoes'>
-                <div className='opn-cliente'>
-                    <h2>Opções</h2>
-                    <ul className="opcoes">
-                        {opcoes.map((opcao, index) => (
-                            <li key={index}>
-                                <span className='icon'>{opcao.icone}</span>
-                                <span className='informacoes'>
-                                    <h3>{opcao.titulo}</h3>
-                                    <p>{opcao.descricao}</p>
-                                </span>
-                            </li>
-                        ))}
-                    </ul>
-                </div>
-            </section>
-        </main>
-    )
+  const opcoes = [
+    {
+      titulo: 'Cadastrar Dispositivo',
+      descricao: 'Cadastre um novo dispositivo LifeCare.',
+      icone: <IoWatchOutline />,
+      rota: '/portalcliente/dispositivo/cadastrar',
+    },
+    {
+      titulo: 'Visualizar Dispositivos',
+      descricao: 'Veja todos os dispositivos LifeCare registrados.',
+      icone: <RxMagnifyingGlass />,
+      rota: '/portalcliente/dispositvo/visualizar',
+    },
+    {
+      titulo: 'Informações do Cuidador',
+      descricao: 'Visualize informações sobre o seu cuidador.',
+      icone: <RiQuestionMark />,
+      rota: '/portalcliente/cuidador/visualizar',
+    },
+    {
+      titulo: 'Gerenciar Diagnósticos',
+      descricao: 'Visualize e gerencie diagnósticos relacionados ao seu dispositivo.',
+      icone: <FaFileMedicalAlt />,
+      rota: '/portalcliente/diagnostico/visualizar',
+    },
+    {
+      titulo: 'Configurações de Conta',
+      descricao: 'Atualize suas informações de conta e preferências.',
+      icone: <FaUserGear />,
+      rota: '/portalcliente/cliente/meudados',
+    },
+    {
+      titulo: 'Visualizar Serviços de Manutenção',
+      descricao: 'Visualize serviços de manutenção para o seu dispositivo.',
+      icone: <GrConfigure />,
+      rota : '/portalcliente/manutencao/visualizar',
+    },
+    {
+      titulo: 'Histórico de Serviços de Emergência',
+      descricao: 'Visualize o histórico de serviços de emergência.',
+      icone: <AiFillAlert />,
+      rota: '/portalcliente/emergencia/visualizar',
+    },
+    {
+      titulo: 'Sair',
+      descricao: 'Encerre a sessão no Portal do Cliente com sergurança.',
+      icone: <ImExit />,
+      rota: '/',
+      onClick: deslogar,
+    },
+  ];
+
+  return (
+    <main>
+      <section className='p-cliente-inicial'>
+        <div className='portal-img'>
+          <img src="/image/pcliente_inicial.jpg" alt="Cuidador e Cliente com LifeCare" />
+        </div>
+        <div className='portal-text'>
+          <h2>Seja bem-vindo ao Portal <br /> do Cliente LifeCare !</h2>
+          <p>Obtenha informações detalhadas sobre o seu dispositivo LifeCare <br /> e gerencie suas opções de maneira simples e intuitiva.</p>
+        </div>
+      </section>
+      <section className='p-cliente-opcoes'>
+        <div className='opn-cliente'>
+          <h2>Opções</h2>
+          <ul className="opcoes">
+            {opcoes.map((opcao, index) => (
+              opcao.rota && (
+                <Link href={opcao.rota} key={index}>
+                  <li onClick={opcao.onClick}>
+                    <span className='icon'>{opcao.icone}</span>
+                    <span className='informacoes'>
+                      <h3>{opcao.titulo}</h3>
+                      <p>{opcao.descricao}</p>
+                    </span>
+                  </li>
+                </Link>
+              )
+            ))}
+          </ul>
+        </div>
+      </section>
+    </main>
+  )
 }
