211d5febef760581d793553e65b4bb6cf4f226d9
laiscrz <lais.albino2003@gmail.com>
2023-11-22 17:19:01 -0300

    add metodo DELETE em manutenção

--- src/app/portalcliente/manutencao/page.jsx
+++ src/app/portalcliente/manutencao/page.jsx
@@ -2,22 +2,41 @@
 import BotaoVoltarMenu from "@/components/botaovoltarmenu";
 import { useEffect, useState } from "react";
 import '/src/app/portalcliente/opcoes.scss'
+import { FaTimesCircle } from "react-icons/fa";
 
 
-export default function VisualizarManutencao(){
+export default function VisualizarManutencao() {
 
     const [manutencoes, setManutencoes] = useState([]);
 
     useEffect(() => {
-        fetch(`http://localhost:5000/manutencao`)
+        fetch(`http://localhost:8080/GlobalJava/api/manutencao`)
             .then(resp => resp.json())
             .then(resp => setManutencoes(resp))
             .catch(error => console.error(error))
     }, [])
 
 
+    const handleDelete = (idServico) => {
+        if (idServico) {
+            fetch(`http://localhost:8080/GlobalJava/api/manutencao/${idServico}`, {
+                method: 'DELETE'
+            })
+                .then(response => {
+                    console.log('Resposta do servidor:', response);
+                    return response.json();
+                })
+                .catch(error => console.error(error))
+                .finally(() => {
+                    alert('Serviço de Manutenção Excluido com Sucesso!')
+                    window.location.reload();
+                });
+        } else {
+            alert('idServico indefinido. Não é possível excluir.');
+        }
+    }
 
-    return(
+    return (
         <main>
             <section className="manutencao-visualizar">
                 <div className="manutencao-img">
@@ -45,7 +64,14 @@ export default function VisualizarManutencao(){
                                     <td>{manutencao.dataManutencao}</td>
                                     <td>{manutencao.status}</td>
                                     <td>{manutencao.custo}</td>
-                                    <td>Opções</td>
+                                    <td>
+                                        <div className="btn-container">
+                                            <button onClick={handleDelete.bind(this, manutencao.idServico)} className='btn-metodo'>
+                                                <FaTimesCircle /> Exluir
+                                            </button>
+                                        </div>
+
+                                    </td>
                                 </tr>
                             ))}
                         </tbody>
