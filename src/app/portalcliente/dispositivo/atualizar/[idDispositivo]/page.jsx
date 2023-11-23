bb0fd3f8de13f2e006ebbd12eae558c0a3312596
laiscrz <lais.albino2003@gmail.com>
2023-11-23 09:06:57 -0300

    add metodo DELETE em dispositivo

---
+++ src/app/portalcliente/dispositivo/atualizar/[idDispositivo]/page.jsx
@@ -0,0 +1,8 @@
+
+export default function AtualizarDispositivo(){
+    return(
+        <main>
+
+        </main>
+    )
+}
--- src/app/portalcliente/dispositivo/page.jsx
+++ src/app/portalcliente/dispositivo/page.jsx
@@ -2,16 +2,38 @@
 import { useEffect, useState } from 'react';
 import '/src/app/portalcliente/opcoes.scss';
 import BotaoVoltarMenu from '@/components/botaovoltarmenu';
+import Link from 'next/link';
+import { FaEdit, FaTimesCircle } from 'react-icons/fa';
 
 export default function VisualizarDispositivo() {
   const [dispositivos, setDispositivos] = useState([]);
 
   useEffect(() => {
-    fetch(`http://localhost:5000/dispositivo`)
-        .then(resp => resp.json())
-        .then(resp => setDispositivos(resp))
+    fetch(`http://localhost:8080/GlobalJava/api/dispositivo`)
+      .then(resp => resp.json())
+      .then(resp => setDispositivos(resp))
+      .catch(error => console.error(error))
+  }, [])
+
+
+  const handleDelete = (idDispositivo) => {
+    if (idDispositivo) {
+      fetch(`http://localhost:8080/GlobalJava/api/dispositivo/${idDispositivo}`, {
+        method: 'DELETE'
+      })
+        .then(response => {
+          console.log('Resposta do servidor:', response);
+          return response.json();
+        })
         .catch(error => console.error(error))
-}, [])
+        .finally(() => {
+          alert('Dispositivo Excluido com Sucesso!')
+          window.location.reload();
+        });
+    } else {
+      alert('idDispositivo indefinido. Não é possível excluir.');
+    }
+  }
 
   return (
     <main>
@@ -19,7 +41,7 @@ export default function VisualizarDispositivo() {
         <div className="dispositivo-img">
           <h2>Dispositivos Cadastrados</h2>
           <img src="/image/visu_disp.png" alt="Ver dispositivos" />
-          <BotaoVoltarMenu/>
+          <BotaoVoltarMenu />
         </div>
         <div className="dispositivo-table">
           <table>
@@ -41,7 +63,18 @@ export default function VisualizarDispositivo() {
                   <td>{dispositivo.status}</td>
                   <td>{dispositivo.cor}</td>
                   <td>{dispositivo.dataFabricacao}</td>
-                  <td>Opções</td> 
+                  <td>
+                    <div className="btn-container">
+                      <Link href="/portalcliente/dispositivo/atualizar/[idDispositivo]" as={`/portalcliente/dispositivo/atualizar/${dispositivo.idDispositivo}`}>
+                        <button className="btn-metodo">
+                          <FaEdit /> Editar Telefone
+                        </button>
+                      </Link>
+                      <button onClick={handleDelete.bind(this, dispositivo.idDispositivo)} className='btn-metodo'>
+                        <FaTimesCircle /> Exluir
+                      </button>
+                    </div>
+                  </td>
                 </tr>
               ))}
             </tbody>
